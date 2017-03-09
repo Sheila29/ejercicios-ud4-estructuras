d3f28f7f2a4eae0816f8d71553180eb835bac4a8
Sheilapsm <murgia85@gmail.com>
2017-03-09 21:52:07 +0100

    empezado

---
+++ 09_averias/src/com/company/Averia.java
@@ -0,0 +1,7 @@
+package com.company;
+
+/**
+ * Created by 7FPROG10 on 09/03/2017.
+ */
+public class Averia {
+}
---
+++ 09_averias/src/com/company/Tecnico.java
@@ -0,0 +1,9 @@
+package com.company;
+
+/**
+ * Created by 7FPROG10 on 09/03/2017.
+ */
+public class Tecnico {
+
+
+}
